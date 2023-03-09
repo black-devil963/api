0acf69d930921337c485045ec1563ed104c6a51f
nikhil kumar aman <nikhilkumaraman963@gmail.com>
2023-03-09 23:27:28 +0530

    msgup

--- routes/container list.js
+++ routes/container list.js
@@ -4,6 +4,7 @@ const shell = require('shelljs')
 router.get("/",function(req,res,next){
 
 //shell.exec('docker start container2');
-    res.send(shell.exec('docker container ls -a'));
+res.send(shell.exec('docker container ls -a ')  )
+// res.send(shell.exec('docker container ls -a --format "<tr><td>{{.ID}}</td><td>{{.Names}}</td><td>{{.Image}}</td><td>{{.Command}}</td><td>{{.CreatedAt}}</td><td>{{.Status}}</td><td>{{.Ports}}</td></tr>#@$@#'));
 });
 module.exports=router;
