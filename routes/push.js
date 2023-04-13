d9d543a00cbc917605069022436448fd859c50bc
nikhil kumar aman <nikhilkumaraman963@gmail.com>
2023-04-14 01:28:44 +0530

    msgpublish1

--- routes/push.js
+++ routes/push.js
@@ -12,13 +12,13 @@ if(req.originalUrl.split('-')[1].length!=0){
     if(t.length==0){res.send("container doesn't exists");return;}
     else cmmd1="docker commit "+req.originalUrl.split('-')[1];
 }
-if(req.originalUrl.split('-')[2].length!=0)cmmd2=req.originalUrl.split('-')[2];else {res.send("image not mentioned");return;}
+if(req.originalUrl.split('-')[2].length!=0)cmmd2=" "+req.originalUrl.split('-')[2];else {res.send("image not mentioned");return;}
 if(req.originalUrl.split('-')[3].length!=0)cmmd2+=":"+req.originalUrl.split('-')[3];else cmmd+=":latest";
 //id=shell.exec(`docker container create ${cmmd}`);
 id=shell.exec(cmmd1+cmmd2);
 if(id.length==0){res.send("Invalid image:tag pair");return;}
 else {
-    id=shell.exec("docker push "+cmmd2);
+    id=shell.exec("docker push"+cmmd2);
     if(id.length==0){res.send("image publishing failed");return;}
     res.send(cmmd2+" created");
 }
