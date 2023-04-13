c623e840593b5d4f555e3b12fe73d623757b473e
nikhil kumar aman <nikhilkumaraman963@gmail.com>
2023-04-14 01:07:16 +0530

    msgpublish1

--- routes/create container.js
+++ routes/create container.js
@@ -4,24 +4,21 @@ var router=express.Router();
 const shell = require('shelljs');
 router.get("/",function(req,res,next){
 
-cmmd1="";
-cmmd2="";
+cmmd="";
 if(req.originalUrl.split('-')[1].length!=0){
     t=shell.exec('docker container ls -a -q --filter name='+req.originalUrl.split('-')[1])
     console.log(t);
-    if(t.length==0){res.send("container doesn't exists");return;}
-    else cmmd1="docker commit "+req.originalUrl.split('-')[1];
+    if(t.length!=0){res.send("Name exists with a container");return;}
+    else cmmd="--name "+req.originalUrl.split('-')[1];
 }
-if(req.originalUrl.split('-')[2].length!=0)cmmd2=" "+req.originalUrl.split('-')[2];else {res.send("image not mentioned");return;}
-if(req.originalUrl.split('-')[3].length!=0)cmmd2+=":"+req.originalUrl.split('-')[3];else cmmd+=":latest";
+if(req.originalUrl.split('-')[2].length!=0)cmmd+=" "+req.originalUrl.split('-')[2];else {res.send("image not mentioned");return;}
+if(req.originalUrl.split('-')[3].length!=0)cmmd+=":"+req.originalUrl.split('-')[3];else cmmd+=":latest";
 //id=shell.exec(`docker container create ${cmmd}`);
-id=shell.exec(cmmd1+cmmd2);
+id=shell.exec(`docker container create -it ${cmmd}`);
+
+console.log(id.length);
 if(id.length==0){res.send("Invalid image:tag pair");return;}
-else {
-    id=shell.exec("docker push "+cmmd2);
-    if(id.length==0){res.send("image publishing failed");return;}
-    res.send(cmmd2+" created");
-}
+else res.send(shell.exec('docker container ls -a --format "{{.ID}}\t{{.Names}}" --filter ID='+id));
 
 
 });
