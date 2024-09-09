58c1783eedd3d9eb51d5c89267f927f1f9104603
black-devil963 <89310171+black-devil963@users.noreply.github.com>
2024-09-10 05:05:48 +0530

    Update InspectNet.js

--- routes/InspectNet.js
+++ routes/InspectNet.js
@@ -2,6 +2,15 @@ var express = require("express");
 var app=express();
 const shell = require('shelljs')
 app.get("/",function(req,res,next){
-    res.send(shell.exec('docker inspect '+req.originalUrl.split('-')[1]));
+    //res.send(shell.exec('docker inspect '+req.originalUrl.split('-')[1]));
+    
+    const array=shell.exec('docker inspect '+req.originalUrl.split('-')[1]+' --format="{{range .Containers}}{{.Name}}#$#{{.IPv4Address}}#$#{{.MacAddress}},{{end}}"').split(',');
+    var result=[]
+    for (var i = 0; i<array.length-1; i++)
+        {
+            
+            result.push(array[i]+"#$#"+shell.exec('docker inspect --format="{{.Config.ExposedPorts}}" '+array[i].split('#$#')[0]));
+        }
+    res.send(result);
 });
 module.exports=app;
